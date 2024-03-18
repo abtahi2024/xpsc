3c28864d8e077694013236fdde8002205d07cb16
Abtahi <163572318+abtahi2024@users.noreply.github.com>
2024-03-18 18:00:03 +0600

    Update A_Wet_Shark_and_Odd_and_Even.cpp

--- w1/day3/A_Wet_Shark_and_Odd_and_Even.cpp
+++ w1/day3/A_Wet_Shark_and_Odd_and_Even.cpp
@@ -1,14 +1,30 @@
 #include<bits/stdc++.h>
+#define ll long long int
 using namespace std;
 int main()
 {
     int n;
     cin>>n;
-    int sum;
-    for(int i=1;i<=n;i++)
+    vector<ll>a(n);
+    ll sum=0;
+    for(int i=0;i<n;i++)
     {
+        cin>>a[i];
+        sum+=a[i];
+    }
+    sort(a.begin(),a.end());
+    if(sum%2==1)
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]%2==1)
+            {
+                sum-=a[i];
+                break;
+            }
+        }
         
     }
-
+    cout<<sum<<endl;
     return 0;
 }
